f0f1f4f17a23f90dc92a9777cf48ef903fe5a69f
Athemsj01 <aldacohdzjuan@outlook.com>
2022-04-24 12:37:20 -0500

    Instalaciones de modulo e implementacion de vista meseros

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-main>
+      <v-row class= "mt-5">
+
+      <v-spacer></v-spacer>
+
+        <router-link :to="{name: 'Home'}" class="mr-5">Menu</router-link>
+
+        <router-link :to="{name: 'Mesero'}" class="mr-5">Mesero</router-link>
+
+      <v-spacer></v-spacer>
+
+      </v-row>
+
+      <router-view>
+
+      </router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
